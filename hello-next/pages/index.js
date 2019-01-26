5981a8ebac23fd9a3e5ca72af5216ccf632b8eef
Connor Clarkson <connor.sqidge@hotmail.co.uk>
2019-01-26 15:25:20 +0000

    Receiving content from an API

--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -1,9 +1,31 @@
-// pages/index.js
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
 
-import Layout from '../comps/MyLayout.js'
-
-export default () => (
+const Index = (props) => (
     <Layout>
-        <p>Hello Next.js</p>
+        <h1>Batman TV Shows</h1>
+        <ul>
+            {props.shows.map(({ show }) => (
+                <li key={show.id}>
+                    <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+                        <a>{show.name}</a>
+                    </Link>
+                </li>
+            ))}
+        </ul>
     </Layout>
 )
+
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+    const data = await res.json()
+
+    console.log(`Show data fetched. Count: ${data.length}`)
+
+    return {
+        shows: data
+    }
+}
+
+export default Index
