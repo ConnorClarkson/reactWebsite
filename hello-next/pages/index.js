7fca9162aa30e0072bd9d6f08f3e127129c6c3f0
Connor Clarkson <connor.sqidge@hotmail.co.uk>
2019-01-26 15:02:54 +0000

    Using components

--- hello-next/pages/index.js
+++ hello-next/pages/index.js
@@ -1,7 +1,9 @@
-const Index = () => (
-    <div>
-        <p>Hello Next.js</p>
-        </div>
+// pages/index.js
 
+import Layout from '../comps/MyLayout.js'
+
+export default () => (
+    <Layout>
+        <p>Hello Next.js</p>
+    </Layout>
 )
-export default Index
