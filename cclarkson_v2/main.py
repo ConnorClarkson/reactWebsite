2442b92d61fa165a1181b575d9885b0ae027dbef
Connor Clarkson <connor.sqidge@hotmail.co.uk>
2019-06-21 16:44:36 +0100

    v2 init

---
+++ cclarkson_v2/main.py
@@ -0,0 +1,11 @@
+from app import app
+
+
+# if __name__ == '__main__':
+#     config=read_config()
+#     if config:
+#         es = connectES(config['elasticsearch'])
+#     else:
+#         print("No Config")
+#         sys.exit(0)
+#     app.run()
